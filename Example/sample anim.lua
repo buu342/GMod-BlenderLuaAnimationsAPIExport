c9dad48c85fc9657eafeffa6987afa4bef125508
Buu342 <buu342@hotmail.com>
2020-02-01 14:53:18 +0000

    Add files via upload

---
+++ Example/sample anim.lua
@@ -0,0 +1,41 @@
+RegisterLuaAnimation('raisearmtest', {
+	FrameData = {
+		{
+			BoneInfo = {
+				['ValveBiped.Bip01_L_UpperArm'] = {
+				},
+				['ValveBiped.Bip01_R_UpperArm'] = {
+				}
+			},
+			FrameRate = 100
+		},
+		{
+			BoneInfo = {
+				['ValveBiped.Bip01_L_UpperArm'] = {
+					RU = -46,
+					RR = -16,
+					RF = -1
+				},
+				['ValveBiped.Bip01_R_UpperArm'] = {
+				}
+			},
+			FrameRate = 6.0
+		},
+		{
+			BoneInfo = {
+				['ValveBiped.Bip01_L_UpperArm'] = {
+					RU = -92,
+					RR = -31,
+					RF = -1
+				},
+				['ValveBiped.Bip01_R_UpperArm'] = {
+					RU = -72,
+					RR = -3,
+					RF = 37
+				}
+			},
+			FrameRate = 6.0
+		},
+	},
+	Type = TYPE_GESTURE
+})
